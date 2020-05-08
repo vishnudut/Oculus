3b06769d3cee8ca4208d562dd6b446b9a8f4fc46
vishnudut <vvdut99@gmail.com>
2020-05-08 16:53:05 +0530

    Fixed the bug on ocr file

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -9,13 +9,20 @@ import os
 import playsound
 import random
 
-
-
 # parse the argument
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--image", required = True)
 parser.add_argument("-p", "--preprocess", type = str, default = "thresh")
 args = vars(parser.parse_args())
+
+def speak(audio_string):
+	tts = gTTS(text=audio_string, lang='en')
+	r = random.randint(1,100000000)
+	audio_file = 'audio-' + str(r) + '.mp3'
+	tts.save(audio_file)
+	playsound.playsound(audio_file)
+	print(audio_string)
+	os.remove(audio_file)
 
 # load the example image and convert it to grayscale
 image = cv2.imread(args["image"])
@@ -41,6 +48,7 @@ os.remove(filename)
 
 #TO-DO : Additional processing such as spellchecking for OCR errors or NLP 
 print(text)
+speak(text)
  
 # show the output images
 # cv2.imshow("Image", image)
