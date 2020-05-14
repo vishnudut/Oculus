c19d1d32ca168f2a056dfa0eab3da28627aa7d11
vishnudut <vvdut99@gmail.com>
2020-05-14 21:30:52 +0530

    Completed the faceEncoding and storing the userEncoding in the databse

--- Oculus_Django/face/admin.py
+++ Oculus_Django/face/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import faceEncodings
 # Register your models here.
+
+admin.site.register(faceEncodings)
--- Oculus_Django/face/faceEncodings.py
+++ Oculus_Django/face/faceEncodings.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from .models import faceEncodings
+
 from imutils import paths
 import face_recognition
 import argparse
 import pickle
 import cv2
 import os
+import json
 import numpy as np
 
 
@@ -22,14 +25,23 @@ def encoding(request):
     uti_formate = np.fromstring(imagebits,np.uint8)
     print(uti_formate)
     img = cv2.imdecode(uti_formate, cv2.IMREAD_COLOR)
-    # np_data = np.asarray(uti_formate)
-    # print('THIS IS NP DTA:', np_data)
+
     rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     boxes = face_recognition.face_locations(rgb,model='hog')
     encodings = face_recognition.face_encodings(rgb, boxes)
-    print(encodings)            
-        
-        
-    return HttpResponse('Encoding')
+    print(encodings)      
+
+    for encoding in encodings:
+        np_array_to_list = list(np.asarray(encoding))
+        json_encoding_list = json.dumps(np_array_to_list)
+        person_name = name
+        user_encoding = json_encoding_list
+
+    store_in_database = faceEncodings.objects.create(user_name=person_name, encodings= user_encoding)
+
+    if store_in_database is not None:
+        return HttpResponse('Success')
+    else:
+        return HttpResponse('Failiure')
 
     
