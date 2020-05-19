fbd7a49dd38d66c1cf8c1e8e644a1e35b0367ffe
vishnudut <vvdut99@gmail.com>
2020-05-19 21:43:36 +0530

    The speak function is currently in the face recognition module. Need to implement it as a seperate unit

--- face/pi_face_recognition.py
+++ face/pi_face_recognition.py
@@ -20,14 +20,14 @@ import random
 # 	help="path to serialized db of facial encodings")
 # args = vars(ap.parse_args())
 
-# def speak(audio_string):
-	# 	tts = gTTS(text=audio_string, lang='en')
-	# 	r = random.randint(1,100000000)
-	# 	audio_file = 'audio-' + str(r) + '.mp3'
-	# 	tts.save(audio_file)
-	# 	playsound.playsound(audio_file)
-	# 	print(audio_string)
-	# 	os.remove(audio_file)
+def speak(audio_string):
+		tts = gTTS(text=audio_string, lang='en')
+		r = random.randint(1,100000000)
+		audio_file = 'audio-' + str(r) + '.mp3'
+		tts.save(audio_file)
+		playsound.playsound(audio_file)
+		print(audio_string)
+		os.remove(audio_file)
 
 
 def whoIsThat:
